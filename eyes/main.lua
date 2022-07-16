796d4a58cadb1f2d48b0753b05f3cc4789da9723
Am1r-B_PC <69463011+Am1r-B@users.noreply.github.com>
2022-07-17 01:03:39 +0430

    start code and draw an eye

---
+++ eyes/main.lua
@@ -0,0 +1,7 @@
+function love.draw()
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.circle("fill", 200, 200, 50)
+  
+  love.graphics.setColor(0, 0, .4)
+  love.graphics.circle("fill", 200, 200, 15)
+end
