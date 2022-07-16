701a9c75351f5d63dd82a92a961fe953d2303ec3
Am1r-B_PC <69463011+Am1r-B@users.noreply.github.com>
2022-07-17 01:38:54 +0430

    make moving pupil

--- eyes/main.lua
+++ eyes/main.lua
@@ -13,7 +13,12 @@ function love.draw()
   -- straight-line distance between cursor and eye
   local distance = math.sqrt(distanceX^2 + distanceY^2)
   
+  -- angle between cursor and eye
   local angle = math.atan2(distanceY, distanceX)
+  
+  -- location of pupil
+  local pupilX = eyeX + distance * math.cos(angle)
+  local pupilY = eyeY + distance * math.sin(angle)
   
   -- draw eye(white part of eye)
   love.graphics.setColor(1, 1, 1)
@@ -21,7 +26,7 @@ function love.draw()
   
   -- draw pupil of eye
   love.graphics.setColor(0, 0, .4)
-  love.graphics.circle("fill", 200, 200, 15)
+  love.graphics.circle("fill", pupilX, pupilY, 15)
   
   -- write current info on screen
   love.graphics.setColor(1, 1, 1)
@@ -29,6 +34,8 @@ function love.draw()
     "Distance X: " .. distanceX,
     "Distance Y: " .. distanceY,
     "Distance: " .. distance,
-    "Angle: " .. angle
+    "Angle: " .. angle,
+    'cos(angle): ' .. math.cos(angle),
+    'sin(angle): ' .. math.sin(angle)
   }, "\n"))
 end
